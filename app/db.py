841296a209dcd6957360f23fe4a48e0d3c229e36
kauan <kauanmartinsesilva@gmail.com>
2026-05-16 18:23:05 -0300

    Deploy

--- app/db.py
+++ app/db.py
@@ -7,16 +7,15 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+# Força o uso do pymysql
+if DATABASE_URL and DATABASE_URL.startswith("mysql"):
+    DATABASE_URL = DATABASE_URL.replace("mysql://", "mysql+pymysql://")
+
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
     pool_recycle=280
 )
 
-SessionLocal = sessionmaker(
-    bind=engine,
-    autoflush=False,
-    autocommit=False
-)
-
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
